1f942f3986cab8fb904038ab6faf9d5106634706
waliid ilham <92657818+waliidilham@users.noreply.github.com>
2023-10-28 18:26:28 +0700

    landing-page

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -220,7 +220,7 @@
 
     <div class="heroe">
 
-        <h1>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
+        <h1>Halo Waliid Ilham Ramadhan (2117051094) <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
 
         <h2>The small framework with powerful features</h2>
 
