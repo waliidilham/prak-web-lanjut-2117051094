f9bd255a6af216017135d210604abf7ecc8e8f3a
waliid ilham <92657818+waliidilham@users.noreply.github.com>
2023-10-28 19:50:22 +0700

    update-delete

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -15,4 +15,8 @@ $routes->get('/user/profile', [UserController::class, 'profile']);
 $routes->get('/user/create', [UserController::class, 'create']);
 $routes->post('/user/store', [UserController::class, 'store']);
 $routes->get('/user', [UserController::class, 'index']);
+$routes->get('user/(:any)/edit', [UserController::class, 'edit']);
+$routes->put('user/(:any)', [UserController::class, 'update']);
+$routes->delete('user/(:any)', [UserController::class, 'destroy']);
+
 $routes->get('user/(:any)', [UserController::class, 'show']);
