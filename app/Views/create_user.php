8a30fe102e0a8f185595d074120c8c469300336c
waliid ilham <92657818+waliidilham@users.noreply.github.com>
2023-11-03 01:46:32 +0700

    form-controller

--- app/Views/create_user.php
+++ app/Views/create_user.php
@@ -1,59 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Form</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+</style>
 </head>
 <body>
-    <form action="<?=base_url('user/store')?>" method="post">
-    <div  style="height:100vh;width:100%;">
-    <div class="rounded" style="padding: 20px; border:5px solid red;">
-      <h2>Form Biodata Diri</h2>
-         <table>
-         <tr>
-             <td>Nama</td>
-             <td>:</td>
-             <td><input type="text" class="form-control mt-2 <?= session('validation') && session('validation')->hasError('nama') ? 'is-invalid' : '' ?>" id="floatingName" placeholder="Nama" name="nama" value="<?= old('nama') ?>">
-             <?php if (session('validation') && session('validation')->hasError('nama')) : ?>
-                <div class="invalid-feedback">
-                     <?= session('validation')->getError('nama'); ?>
-                </div>
-            <?php endif; ?>
-            </td> 
-         </tr>
-         <tr>
-             <td>NPM</td>
-             <td>:</td>
-             <td><input type="number" class="form-control mt-2 <?= session('validation') ? 'is-invalid' : '' ?>" id="floatingNpm" placeholder="NPM" name="npm" value="<?= old('npm') ?>">
-             <?php if (session('validation') && session('validation')->hasError('npm')) : ?>
-                <div class="invalid-feedback">
-                    <?= session('validation')->getError('npm'); ?>
-                </div>
-            <?php endif; ?>
-            </td>
-         </tr>
-         <tr>
-            <td>Kelas</td>
-            <td>:</td>
-            <td>
-                <select name="kelas" id="kelas">
-                    <?php foreach($kelas as $item):?>
-                        <option value="<?=$item['id']?>"><?=$item['nama_kelas']?></option>
-                    <?php endforeach;?>
-                </select>
-            </td>
-        </tr>
-         <tr>
-             <td>Jurusan</td>
-             <td>:</td>
-             <td><input type="text" name="jurusan"></td>
-         </tr>
-         
-         <tr>
-             <td><input type="submit" value="Simpan"></td>
-         </tr>
-     </form>
+  <main class="form-signin w-100 m-auto">
+   <div class="container-sm">
+   <form method="POST" action="<?=base_url('/user/store')?>">
+      <h1 class="h3 mt-5 mb-3 fw-normal text-center">Sign Up</h1>
+
+      <div class="form-floating">
+        <input type="text" class="form-control mt-2" id="floatingName" placeholder="Nama" name="nama">
+        <label for="floatingName">Nama</label>
+      </div>
+      <div class="form-floating">
+        <input type="number" class="form-control mt-2" id="floatingNpm" placeholder="NPM" name="npm">
+        <label for="floatingNpm">NPM</label>
+      </div>
+      <div class="form-floating">
+        <input type="text" class="form-control mt-2" id="floatingKelas" placeholder="Kelas" name="kelas">
+        <label for="floatingKelas">Kelas</label>
+      </div>
+      <button class="btn btn-success sd-grid gap-2 col-6 mx-auto" type="submit">Insert it</button>
+    </form>
+   </div>
+  </main>
 </body>
 </html>
